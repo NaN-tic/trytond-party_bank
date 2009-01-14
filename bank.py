feda5f1ca1ab6ff8b2995d556d826a946c883d54
Korbinian Preisler <timitos@virtual-things.biz>
2009-01-14 20:27:20 +0100

    Guidelines

--- bank.py
+++ bank.py
@@ -7,12 +7,12 @@ class Bank(OSV):
     _name = 'bank.bank'
     _description = __doc__
     _inherits = {'party.party': 'party'}
-    
+
     party = fields.Many2One('party.party', 'Party', required=True,
             ondelete='CASCADE')
     bank_code = fields.Char('National Code', select=1)
     bic = fields.Char('BIC/SWIFT', select=1)
-        
+
 Bank()
 
 
@@ -20,24 +20,24 @@ class BankAccount(OSV):
     'Bank Account'
     _name = 'bank.account'
     _description = __doc__
-    
+
     name = fields.Char('Name', required=True)
     code = fields.Char('Account Number', help='National Standard Code')
     iban = fields.Char('IBAN')
     bank = fields.Many2One('bank.bank', 'Bank', required=True)
     currency = fields.Many2One('currency.currency', 'Currency', required=True)
-    party = fields.Many2One('party.party', 'Party', 
+    party = fields.Many2One('party.party', 'Party',
                             ondelete='CASCADE', required=True)
     owner = fields.Char('Differing Owner')
-    
-BankAccount()    
-    
-    
+
+BankAccount()
+
+
 class Party(OSV):
     'Party'
     _name = 'party.party'
     _description = __doc__
-    
+
     bank_accounts = fields.One2Many('bank.account', 'party', 'Bank Accounts')
-        
+
 Party()
