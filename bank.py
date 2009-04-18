9e88db33ac5952088ee6268e2d7a08ae0564853c
Korbinian Preisler <timitos@virtual-things.biz>
2009-04-18 14:03:23 +0200

    extend search_rec_name for search with bic

--- bank.py
+++ bank.py
@@ -33,7 +33,13 @@ class Bank(ModelSQL, ModelView):
             if ids:
                 args2.append(('bank_code', args[i][1], args[i][2]))
             else:
-                args2.append((self._rec_name, args[i][1], args[i][2]))
+                ids = self.search(cursor, user, [
+                             ('bic', args[i][1], args[i][2]),
+                             ], limit=1, context=context)
+                if ids:
+                    args2.append(('bic', args[i][1], args[i][2]))
+                else:
+                    args2.append((self._rec_name, args[i][1], args[i][2]))
             i += 1
         return args2
 
