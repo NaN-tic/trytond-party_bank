31db069bffea8586714071c844d9194f0b0262f2
Mathias Behrle <mathiasb@behrle.dyndns.org>
2009-04-30 00:13:34 +0200

    Update translation for de_DE

--- __tryton__.py
+++ __tryton__.py
@@ -8,7 +8,7 @@
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
-    'description': '''WARNING: BETA STATUS
+    'description': '''        WARNING: BETA STATUS
         This module is in public testing phase and not yet released.
         Never use this module in productive environment. You can not
         uninstall this module once it is installed. Watch
@@ -18,8 +18,20 @@
         http://bugs.tryton.org. Please note your testing results on
         http://code.google.com/p/tryton/wiki/Testing1_2_0#External_Modules.
         Allows the management of bank accounts for parties''',
-    #'description_de_DE': '''Ermöglicht die Verwaltung von Bankverbindungen für Parteien
-#''',
+    'description_de_DE': '''        ACHTUNG: MODUL IM BETA STATUS
+        Dieses Modul befindet sich in der öffentlichen Testphase und
+        ist noch nicht released. Benutzen Sie dieses Modul nicht in
+        produktiven Umgebungen. Das Modul kann nicht mehr deinstalliert werden,
+        nachdem es in eine Datenbank installiert wurde.
+        Release Ankündigung werden auf www.tryton.org/news.html veröffentlicht.
+
+        Benutzen Sie dieses Modul nur für Testzwecke und unterbreiten Sie
+        Fehler oder Vorschläge auf http://bugs.tryton.org. Bitte vermerken Sie
+        Ihre Testergebnisse auf
+        http://code.google.com/p/tryton/wiki/Testing1_2_0#External_Modules.
+
+    Ermöglicht die Verwaltung von Bankverbindungen für Parteien
+''',
     #'description_es_ES': '''Banco de terceros
     #Permite la gestión de cuentas bancarias de terceros
 #''',
