5c63bf98bd226461fe0202935e5caf70a580f3c2
Mathias Behrle <mathiasb@mbsolutions.selfip.biz>
2010-01-23 21:57:18 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -4,7 +4,7 @@
     'name': 'BETA: Party Bank',
     'name_de_DE': 'BETA: Partei Bankverbindungen',
     'name_es_ES': 'BETA: Banco de terceros',
-    'version': '1.1.0',
+    'version': '1.5.0',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
