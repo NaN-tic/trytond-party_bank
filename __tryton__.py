5cef0cfd3b4be7a18ed54f363047b1fb382eec21
Mathias Behrle <mbehrle@m9s.biz>
2011-11-14 19:22:16 +0100

    Increase version number

--- __tryton__.py
+++ __tryton__.py
@@ -4,7 +4,7 @@
     'name': 'Party Bank',
     'name_de_DE': 'Partei Bankverbindungen',
     'name_es_ES': 'Banco de terceros',
-    'version': '2.0.0',
+    'version': '2.1.0',
     'author': 'virtual things',
     'email': 'info@virtual-things.biz',
     'website': 'http://www.virtual-things.biz/',
